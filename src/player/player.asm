910667c6d79d371b4b5f0d5ee95dfd5b63c022d6
Francisco Langner <Fugante@DESKTOP-LNJLT10.>
2025-09-30 14:07:44 -0600

    implemented fixed point numbers to control player's velocity

--- src/player/player.asm
+++ src/player/player.asm
@@ -2,12 +2,15 @@
 .include "../io.asm"
 
 .importzp tmp1
+.importzp tmp2
 .importzp player_x
 .importzp player_y
 .importzp target_velocity_x
 .importzp target_velocity_y
 .importzp player_velocity_x
 .importzp player_velocity_y
+.importzp player_sprite_x
+.importzp player_sprite_y
 .importzp player_sprite_attrs
 .importzp joy1_down
 
@@ -16,9 +19,18 @@
 .code
 .scope Player
     .scope Initial
-        player_x = SCREEN_WIDTH / 2
-        player_y = SCREEN_HEIGHT - 10
+        player_x0 = %00001000
+        player_x1 = %00000000
+        player_y = SCREEN_HEIGHT - 16
         player_sprite_attrs = $00
+    .endscope
+
+    ; Velocities expressed in fixed point values 4.4
+    .scope Velocities
+        positive:
+            .byte %00010000, %01000000  ; 1.0 and 4.0 pixels per frame
+        negative:
+            .byte %11110000, %11000000  ; 2's complement of positive velocities
     .endscope
 
     .enum Heading
@@ -39,380 +51,152 @@
     .endproc
 
     .proc init_x
-        lda #Initial::player_x
+        lda #Initial::player_x0
         sta player_x
+        lda #Initial::player_x1
+        sta player_x + 1
+    @done:
         rts
     .endproc
 
     .proc init_y
         lda #Initial::player_y
         sta player_y
+    @done:
         rts
     .endproc
 
     .proc init_sprites
         lda #Initial::player_sprite_attrs
         sta player_sprite_attrs
+    @done:
         rts
     .endproc
 
     .scope Movement
         .proc update
-            jsr set_target_velocity
+            jsr set_target_velocity_x
             jsr accelerate
             jsr move
+            jsr bound_position_x
+        @done:
             rts
         .endproc
 
-        .proc set_target_velocity
+        .proc set_target_velocity_x
             ldx #$00
             lda #BTN_B
             and joy1_down
-            beq @no_direction
+            beq @check_right
             inx
-        @no_direction:
-            lda #$00
-            sta target_velocity_x
-            sta target_velocity_y
         @check_right:
             lda #BTN_RIGHT
             and joy1_down
             beq @check_left
-            lda positive_velocity, x
+            lda Velocities::positive, x
             sta target_velocity_x
-            jmp @check_up
+            jmp @done
         @check_left:
             lda #BTN_LEFT
             and joy1_down
-            beq @check_up
-            lda negative_velocity, x
+            beq @no_direction
+            lda Velocities::negative, x
             sta target_velocity_x
-        @check_up:
-            lda #BTN_UP
-            and joy1_down
-            beq @check_down
-            lda negative_velocity, x
-            sta target_velocity_y
             jmp @done
-        @check_down:
-            lda #BTN_DOWN
-            and joy1_down
-            beq @done
-            lda positive_velocity, x
-            sta target_velocity_y
+        @no_direction:
+            lda #$00
+            sta target_velocity_x
         @done:
             rts
-
-        positive_velocity:
-            .byte %00000001, %00000100
-        negative_velocity:
-            .byte %11111111, %11111100   ; 2's complement of positive_velocity
         .endproc
 
         .proc accelerate
-            lda target_velocity_x
-            bmi @negative
-        @positive:
-            cmp player_velocity_x
-            bcc @done
-            beq @done
-            inc player_velocity_x
+            lda player_velocity_x
+            sec
+            sbc target_velocity_x
+            bne @check_greater
             jmp @done
-        @negative:
-            cmp player_velocity_x
-            bcc @done
-            beq @done
+        @check_greater:
+            bmi @lesser
             dec player_velocity_x
+            jmp @done
+        @lesser:
+            inc player_velocity_x
         @done:
             rts
         .endproc
 
-; positive:
-; pv<tv  zc   pv=tv  zc  pv>tv  zc
-;   0100        0100       0100
-;   0001        0100       0111
-; - ----      - ----     - ----
-;   0011 00     0000 11    1101 01
-; 
-; negative:
-;   1100       1100        1100
-;   1111       1100        1001
-; - ----     - ----      - ----
-;   1101 01    0000 11     0011 00
-; 
-; 0010 (2)
-; 1110 (2 two's complement)
-; 
-; -1
-; 
-; 1101 (x two's complement)
-; 0011 (3)
-
-;             lda player_velocity_x
-;             bmi @negative
-;         @positive:
-;             ; if moving right only accelerate if the target velocity is higher
-;             cmp target_velocity_x
-;             bcc @accelerate
-;             jmp @done
-;         @negative:
-;             ; similarly , if moving left only accelerate if target velocity is lower
-;             cmp target_velocity_x
-;             bcs @accelerate
-;             jmp @done
-;         @accelerate:
-;             lda target_velocity_x
-;             sta player_velocity_x
-;         @accelerate:
-;             ; Subtract the current velocity from the target velocity to compare the
-;             ; two values.
-;             ; If V - T == 0:
-;             ;   Then the current velocity is at the target and we are done.
-;             ; If V - T < 0:
-;             ;   Then the velocity is greater than the target and should be decreased.
-;             ; Otherwise, if V - T > 0:
-;             ;   Then the velocity is less than the target and should be increased.
-;             lda player_velocity_x
-;             sec
-;             sbc target_velocity_x
-;             bne @check_greater
-;             jmp @done
-;         @check_greater:
-;             bmi @lesser
-;             dec player_velocity_x
-;         @lesser:
-;             inc player_velocity_x
-;         @done:
-;             rts
-;         .endproc
-
         .proc move
             lda player_velocity_x
+            bmi @negative
+        @positive:
             clc
+            adc player_x + 1
+            sta player_x + 1
+            lda #$00
             adc player_x
             sta player_x
-            lda player_velocity_y
-            clc
-            adc player_y
-            sta player_y
+            jmp @done
+        @negative:
+            lda #$00
+            sec
+            sbc player_velocity_x
+            sta tmp1
+            lda player_x + 1
+            sec
+            sbc tmp1
+            sta player_x + 1
+            lda player_x
+            sbc #$00
+            sta player_x
+        @done:
             rts
         .endproc
 
-;         .proc update
-;             jsr set_target_velocity
-;             jsr accelerate
-;             jsr move
-;             jsr update_motion
-;             rts
-;         .endproc
-; 
-;         .proc set_target_velocity
-;             ; check if the B button is being pressed down and save the state in the X
-;             ; register
-;             ldx #$00
-;             lda #BTN_B
-;             and joy1_down
-;             beq @check_right
-;             inx
-;         @check_right:
-;             lda #btn_right
-;             and joy1_down
-;             beq @check_left
-;             lda positive_velocity, x
-;             sta target_velocity_x
-;             jmp @check_up
-;         @check_left:
-;             lda #BTN_LEFT
-;             and joy1_down
-;             beq @check_up
-;             lda negative_velocity, x
-;             sta target_velocity_x
-;         @check_up:
-;             lda #BTN_UP
-;             and joy1_down
-;             beq @check_down
-;             lda negative_velocity, x
-;             sta target_velocity_y
-;             jpm @done
-;         @check_down:
-;             lda #BTN_DOWN
-;             and joy1_down
-;             beq @no_direction
-;             lda positive_velocity, x
-;             sta target_velocity_y
-;             jmp @done
-;         @no_direction:
-;             lda #$00
-;             sta target_velocity_x
-;             sta target_velocity_y
-;         @done:
-;             rts
-; 
-;         positive_velocity:
-;             .byte $18, $28
-;         negative_velocity:
-;             .byte $e8, $d8
-;         .endproc
-; 
-;         .proc accelerate_x
-;             lda player_velocity_x
-;             bmi @negative
-;         @positive:
-;             cmp target_velocity_x
-;             bcc @accelerate
-;             jmp @done
-;         @negative:
-;             cmp target_velocity_x
-;             bcs @accelerate
-;             jmp @done
-;         @accelerate:
-;             lda player_velocity_x
-;             sec
-;             sbc target_velocity_x
-;             bne @check_greater
-;         @check_greater:
-;             bmi @lesser
-;             dec player_velocity_x
-;             jmp @done
-;         @lesser:
-;             inc player_velocity_x
-;         @done:
-;             rts
-;         .endproc
-; 
-;         .proc accelerate_y
-;             lda player_velocity_y
-;             bmi @negative
-;         @positive:
-;             cmp target_velocity_y
-;             bcc @accelerate
-;             jmp @done
-;         @negative:
-;             cmp target_velocity_y
-;             bcs @accelerate
-;             jmp @done
-;         @accelerate:
-;             lda player_velocity_y
-;             sec
-;             sbc target_velocity_y
-;             bne @check_greater
-;         @check_greater:
-;             bmi @lesser
-;             dec player_velocity_y
-;             jmp @done
-;         @lesser:
-;             inc player_velocity_y
-;         @done:
-;             rts
-;         .endproc
-; 
-;         .proc update_position
-;             ; Check to see if we're moving to the right (positive) or the left (negative)
-;             lda player_velocity_x
-;             bmi @negative
-;         @positive:
-;             ; Positive velocity is easy: just add the 4.4 fixed point velocity to the
-;             ; 12.4 fixed point position.
-;             clc
-;             adc player_x
-;             sta player_x
-;             lda #$00
-;             adc player_x + 1
-;             sta player_x + 1
-;             jmp @done
-;         @negative:
-;             lda #0
-;             sec
-;             sbc player_velocity_x
-;             sta 
-; 
-; 
-;             @negative:
-;             ; There's probably a really clever way to do this just with ADC but I am
-;             ; lazy and conceptually it made things easier in my head to invert the
-;             ; negative velocity and use SBC.
-;             lda #0
-;             sec
-;             sbc velocityX
-;             sta $00
-;             lda positionX
-;             sec
-;             sbc $00
-;             sta positionX
-;             lda positionX+1
-;             sbc #0
-;             sta positionX+1
-;             rts
-;         .endproc
-; 
-;         .proc update_motion
-;             ; check left direction
-;             lda joy1_down           ; load button presses
-;             and #BTN_LEFT           ; filter out all but left;
-;             beq @check_right        ; if (A == 0) { @check_right } (left is not pressed)
-; 
-;             dec player_x
-;             jmp @check_up
-; 
-;         @check_right:
-;             lda joy1_down
-;             and #BTN_RIGHT
-;             beq @check_up           ; if (A == 0) { @check_up } (right is not pressed)
-; 
-;             inc player_x
-; 
-;         @check_up:
-;             lda joy1_down
-;             and #BTN_UP
-;             beq @check_down         ; if (A == 0) { @check_down } (up is not pressed)
-; 
-;             dec player_y
-;             jmp @done
-; 
-;         @check_down:
-;             lda joy1_down
-;             and #BTN_DOWN
-;             beq @done               ; if (A != 0) { @done } (down is not pressed)
-; 
-;             inc player_y
-; 
-;         @done:
-;             rts
-;         .endproc
-; 
         .proc check_limits
             ; check left
             lda player_x
             cmp #SCREEN_LEFT_LIMIT
             bcs @check_right        ; if (player_x > left limit) { @check_right }
-
             ldx #SCREEN_LEFT_LIMIT - 1
             stx player_x
             jmp @check_up
-
         @check_right:
             cmp #SCREEN_RIGHT_LIMIT
             BCC @check_up           ; if (player_x < right limit) { @check_up }
-
             ldx #SCREEN_RIGHT_LIMIT
             stx player_x
-
         @check_up:
             lda player_y
             cmp #SCREEN_UPPER_LIMIT
             bcs @check_down         ; if (player_y > upper limit) { @check_down }
-
             ldx #SCREEN_UPPER_LIMIT
             stx player_y
             jmp @done
-
         @check_down:
             cmp #SCREEN_LOWER_LIMIT
             BCC @done               ; if (player_y < lower limit) { @done }
-
             ldx #SCREEN_LOWER_LIMIT
             stx player_y
+        @done:
+            rts
+        .endproc
 
+        .proc bound_position_x
+            lda player_x
+            sta tmp1
+            lda player_x + 1
+            sta tmp2
+            asl tmp2
+            rol tmp1
+            asl tmp2
+            rol tmp1
+            asl tmp2
+            rol tmp1
+            asl tmp2
+            rol tmp1
+            lda tmp1
+            sta player_sprite_x
         @done:
             rts
         .endproc
@@ -430,7 +214,7 @@
             sta $0205                   ; byte 1: sprite number
             lda player_sprite_attrs
             sta $0206                   ; byte 2: attributes
-            lda player_x
+            lda player_sprite_x
             sec
             sbc #OFFSET_2x2
             sta $0207                   ; byte 3: x position
@@ -444,7 +228,7 @@
             lda player_sprite_attrs
             eor #%01000000              ; flip sprite horizontally
             sta $020a
-            lda player_x
+            lda player_sprite_x
             sta $020b
             ; bottom left tile (x - 7, y)
             inx
@@ -454,7 +238,7 @@
             sta $020d
             lda player_sprite_attrs
             sta $020e
-            lda player_x
+            lda player_sprite_x
             sec
             sbc #OFFSET_2x2
             sta $020f
@@ -466,17 +250,19 @@
             lda player_sprite_attrs
             eor #%01000000              ; flip sprite horizontally
             sta $0212
-            lda player_x
+            lda player_sprite_x
             sta $0213
-
+        @done:
             rts
         .endproc
     .endscope
 
     .proc update_player
         jsr Movement::update
-        jsr Movement::check_limits
+        nop
+        ;jsr Movement::check_limits
         jsr Sprite::update
+    @done:
         rts
     .endproc
 
--- src/ram.asm
+++ src/ram.asm
@@ -9,41 +9,43 @@ tmp1: .res 1                        ; $0003
 tmp2: .res 1                        ; $0004
 addr1: .res 2                       ; $0005 - $0006
 seed: .res 2                        ; $0007 - $0008
-player_x: .res 1                    ; $0009
-player_y: .res 1                    ; $000a
-target_velocity_x: .res 1           ; $000b
-target_velocity_y: .res 1           ; $000c
-player_velocity_x: .res 1           ; $000d
-player_velocity_y: .res 1           ; $000e
-player_sprite_attrs: .res 1         ; $000f
-ppu_scroll_y: .res 1                ; $0010
-ppu_scroll_x: .res 1                ; $0011
-current_ppu_ctrl: .res 1            ; $0012
-current_ppu_mask: .res 1            ; $0013
+player_x: .res 2                    ; $0009 - $000a
+player_y: .res 2                    ; $000b - $000c
+target_velocity_x: .res 1           ; $000d
+target_velocity_y: .res 1           ; $000e
+player_velocity_x: .res 1           ; $000f
+player_velocity_y: .res 1           ; $0010
+player_sprite_x: .res 1             ; $0011
+player_sprite_y: .res 1             ; $0012
+player_sprite_attrs: .res 1         ; $0013
+ppu_scroll_y: .res 1                ; $0014
+ppu_scroll_x: .res 1                ; $0015
+current_ppu_ctrl: .res 1            ; $0016
+current_ppu_mask: .res 1            ; $0017
 ; s p - - - - - -
 ; | +------------------------------ 0 normal, 1 paused
 ; +-------------------------------- 0 working, 1 sleeping
-game_state: .res 1                  ; $0014
-joy1_press: .res 1                  ; $0015
-joy2_press: .res 1                  ; $0016
-joy1_down: .res 1                   ; $0017
-joy2_down: .res 1                   ; $0018
+game_state: .res 1                  ; $0018
+joy1_press: .res 1                  ; $0019
+joy2_press: .res 1                  ; $001a
+joy1_down: .res 1                   ; $001b
+joy2_down: .res 1                   ; $001c
 ; enemy object pool
-enemy_x_pos: .res NUM_ENEMIES       ; $0019 - $001d
-enemy_y_pos: .res NUM_ENEMIES       ; $001e - $0022
-enemy_x_vels: .res NUM_ENEMIES      ; $0023 - $0027
-enemy_y_vels: .res NUM_ENEMIES      ; $0028 - $002c
-enemy_flags: .res NUM_ENEMIES       ; $002d - $0031
+enemy_x_pos: .res NUM_ENEMIES       ; $001d - $0021
+enemy_y_pos: .res NUM_ENEMIES       ; $0022 - $0026
+enemy_x_vels: .res NUM_ENEMIES      ; $0027 - $002b
+enemy_y_vels: .res NUM_ENEMIES      ; $002c - $0030
+enemy_flags: .res NUM_ENEMIES       ; $0031 - $0035
 ; a _ _ _ _ t t t
 ; |         +-+-+------------------- enemy type (8 posible enemies)
 ; +---------------------------------  0 inactive, 1 active
-current_enemy: .res 1               ; $0032
-current_enemy_type: .res 1          ; $0033
-enemy_timer: .res 1                 ; $0034
-enemy_sprite_attrs: .res 1          ; $0035
+current_enemy: .res 1               ; $0036
+current_enemy_type: .res 1          ; $0037
+enemy_timer: .res 1                 ; $0038
+enemy_sprite_attrs: .res 1          ; $0039
 ; player bullet pool
-bullet_xs: .res NUM_BULLETS         ; $0036 - $0038
-bullet_ys: .res NUM_BULLETS         ; $0039 - $003b
+bullet_xs: .res NUM_BULLETS         ; $003a - $003c
+bullet_ys: .res NUM_BULLETS         ; $003d - $003f
 
 .exportzp tmpA
 .exportzp tmpX
@@ -58,6 +60,8 @@ bullet_ys: .res NUM_BULLETS         ; $0039 - $003b
 .exportzp target_velocity_y
 .exportzp player_velocity_x
 .exportzp player_velocity_y
+.exportzp player_sprite_x
+.exportzp player_sprite_y
 .exportzp player_sprite_attrs
 .exportzp ppu_scroll_x
 .exportzp ppu_scroll_y
